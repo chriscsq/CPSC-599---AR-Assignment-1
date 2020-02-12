3baa55c88ba45f16b58fdb01f314dec82208b7e3
chris <chrischau94@gmail.com>
2020-02-12 02:21:26 -0700

    started project, wasd movement working
    started project, wasd movement working

---
+++ Assets/Scripts/IdleToWalk.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IdleToWalk : MonoBehaviour
+{
+    public Animator animator;
+    public float InputX;
+    public float InputY;
+    public float speed = 50;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = this.gameObject.GetComponent<Animator>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        InputX = Input.GetAxis("Horizontal");
+        InputY = Input.GetAxis("Vertical");
+        animator.SetFloat("InputX", InputX);
+        animator.SetFloat("InputY", InputY);
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(Vector3.up * speed * Time.deltaTime);
+
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(-Vector3.up * speed * Time.deltaTime);
+        }
+    }
+
+
+
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("buff"))
+        {
+            Debug.Log("entered");
+            collision.gameObject.SetActive(false);
+        }
+    }
+
+}
